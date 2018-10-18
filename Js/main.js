31d5f027de83737cad31c12f613eccf9b95868ce
Mini_Bog <595811@pdsb.net>
2018-10-18 13:51:48 -0400

    Change 1.0

--- Js/main.js
+++ Js/main.js
@@ -2,4 +2,12 @@ function test1(){
 
 	var fName = prompt("Hey what's your name?");
 	var myColour = prompt("Pick a colour, " + fName);
+
+	if(myColour == "red"){
+
+		console.log("ha. this guy likes red");
+	}
+	else if( myColour == "green"){
+		console.log()"ah, green. He's alright."): 
+	}
 }
