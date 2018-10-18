3e4275f7d60704c5a9838d5863e3084d1b5f36ef
MiniBog7714 <43042688+MiniBog7714@users.noreply.github.com>
2018-10-18 14:10:00 -0400

    new button

--- Js/main.js
+++ Js/main.js
@@ -7,7 +7,35 @@ function test1(){
 
 		console.log("ha. this guy likes red");
 	}
-	else if( myColour == "green"){
-		console.log()"ah, green. He's alright."): 
+	else if(myColour == "green"){
+		console.log("ah, green. He's alright.");
+	}
+	else if(myColour == "yellow"){
+		console.log("het him something yellow.");
+	}
+	else {
+		console.log("that's not a color")
+	}
+}
+
+function test2(){
+	var age=prompt("how old are you?")
+	
+
+	if(age < 14){
+		alert("GET OUUUUTTT")
+
+	}
+	else if(age == 14){
+		alert("14 is a good age")
+
+	}
+	else if(age > 14){
+		alert("YOU ARE IIIINNNN")
+
+	}
+	else{
+		alert("Not a valid age")
+
 	}
 }
